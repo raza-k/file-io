cf10fb1a5ee7218cd70222e1337d4244e460175b
Raza Khan <raza125@outlook.com>
2021-04-03 00:12:40 -0700

    saving work

---
+++ name.py
@@ -0,0 +1,8 @@
+first = ["john", "fortnite", "monkey"]
+last = ["donkey", "dog", "man"]
+
+for i in range(len(first)):
+    z = first[i]
+    for x in range(len(last)):
+        v = last[x]
+        print(z,v)
